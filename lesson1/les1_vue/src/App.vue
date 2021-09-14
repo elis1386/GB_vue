335ca25d105c687efc67fdfdb61019964c483ce7
Elizaveta <elis1386@gmail.com>
2021-09-14 14:01:19 +0300

    added lesson1

---
+++ lesson1/les1_vue/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+
+  <div id="app">
+    <div class="display">
+       <input v-model="operand1"/>
+       <input v-model="operand2"/>
+       = {{result}}
+     </div>
+
+     <div class="keyboard">
+      <button @click="result = operand1 + operand2">+</button>
+      <button @click="result = operand1 - operand2">-</button>
+      <button @click="result = operand1 / operand2">/</button>
+      <button @click="result = operand1 * operand2">*</button>
+      <button @click="result = operand1 ** operand2">**</button>
+     </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+   name: 'Calculator',
+   data(){
+       return {
+           operand1: 0,
+           operand2: 0,
+           result: 0
+       }
+   }
+ }
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

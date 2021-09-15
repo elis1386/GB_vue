eee901d1dd1bd9e1f306874a3b9f147340c0be05
Elizaveta <elis1386@gmail.com>
2021-09-15 14:12:48 +0300

    added module method

--- lesson1/les1_vue/src/App.vue
+++ lesson1/les1_vue/src/App.vue
@@ -2,17 +2,20 @@
 
   <div id="app">
     <div class="display">
+      <h2>Calculator</h2>
+      
        <input v-model="operand1"/>
        <input v-model="operand2"/>
        = {{result}}
      </div>
 
      <div class="keyboard">
-      <button @click="result = operand1 + operand2">+</button>
-      <button @click="result = operand1 - operand2">-</button>
-      <button @click="result = operand1 / operand2">/</button>
-      <button @click="result = operand1 * operand2">*</button>
-      <button @click="result = operand1 ** operand2">**</button>
+      <button @click="plusMethod">+</button>
+      <button @click="minusMethod">-</button>
+      <button @click="divisionMethod">/</button>
+      <button @click="multiplecatMethod">*</button>
+      <button @click="powerMethod">**</button>
+       <button @click="moduloMethod">%</button>
      </div>
   </div>
 
@@ -27,6 +30,26 @@ export default {
            operand2: 0,
            result: 0
        }
+   },
+   methods: {
+     plusMethod() {
+       this.result = this.operand1 + this.operand2
+     },
+     minusMethod(){
+       this.result = this.operand1 - this.operand2
+     },
+      divisionMethod(){
+       this.result = this.operand1 / this.operand2
+     },
+      multiplecatMethod(){
+       this.result = this.operand1 * this.operand2
+     },
+      powerMethod(){
+       this.result = this.operand1 ** this.operand2
+     },
+      moduloMethod(){
+       this.result = this.operand1 % this.operand2
+     },
    }
  }
 

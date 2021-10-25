fafb2b71e160b3ccf74827f153b17c787d95adaf
Elizaveta <elis1386@gmail.com>
2021-10-25 14:17:20 +0300

    modify files

--- Vue/src/router/categories.js
+++ Vue/src/router/categories.js
@@ -2,42 +2,57 @@ const categories = [
    {
        path: '/payment',
        name: 'Payment',
-       component: () => import('../components/TaskList.vue')
+       component: () => import('../components/TaskList.vue'),
+       props: true
+
    },   
    {
     path: '/Food?amount=:amount',
     name: 'Food',
-    component: () => import('../components/TaskList.vue')
+    component: () => import('../components/TaskList.vue'),
+    props: true
+
 },         
    {
        path: '/Food',
        name: 'Food',
-       component: () => import('../components/TaskList.vue')
+       component: () => import('../components/TaskList.vue'),
+       props: true
+
    },  
    {
        path: '/Transport?amount=:amount',
        name: 'Transport',
-       component: () => import('../components/TaskList.vue')
+       component: () => import('../components/TaskList.vue'),
+       props: true
+
 },    
    {
        path: '/Transport',
        name: 'Transport',
-       component: () => import('../components/TaskList.vue')
+       component: () => import('../components/TaskList.vue'),
+       props: true
+
    }, 
    {
        path: '/Entertaiment?amount=:amount',
        name: 'Entertaiment',
-       component: () => import('../components/TaskList.vue')
+       component: () => import('../components/TaskList.vue'),
+       props: true
+
 },        
    {
        path: '/Entertaiment',
        name: 'Entertaiment',
-       component: () => import('../components/TaskList.vue')
+       component: () => import('../components/TaskList.vue'),
+       props: true
+
    },                    
    {
        path: '/404',
        name: 'ErrorPage',
-       component: () => import('../components/ErrorPage.vue')
+       component: () => import('../components/ErrorPage.vue'),
+
      },
      {
        path: '*',
